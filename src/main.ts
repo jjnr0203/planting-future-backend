8ecf1c740adc8916fb54862d5809d2cdf7dc47e9
jjrn0203 <jonathan08192016@gmail.com>
2025-02-10 16:01:41 -0500

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.setGlobalPrefix("api/v1");
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +14,7 @@ async function bootstrap() {
       transform: true,
     })
   );
+
   await app.listen(3000);
 }
 bootstrap();
